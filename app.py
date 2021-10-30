954f791943c5f9b71809cb540be04cf0651c8378
Huseyn Mammadov <huseynmv@yahoo.com>
2021-10-30 14:11:16 +0400

    Web app without framework

--- app.py
+++ app.py
@@ -1,50 +1,47 @@
 import cgi
 
+import sqlite3
+
+conn= sqlite3.connect('user.db')
+
 form = '''
 <html lang="en">
   <head>
     <!-- Required meta tags -->
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-
     <!-- Bootstrap CSS -->
     <!-- https://cdnjs.com/libraries/twitter-bootstrap/5.0.0-beta1 -->
     <link
       rel="stylesheet"
       href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.0-beta1/css/bootstrap.min.css"
     />
-
     <!-- Icons: https://getbootstrap.com/docs/5.0/extend/icons/ -->
     <!-- https://cdnjs.com/libraries/font-awesome -->
     <link
       rel="stylesheet"
       href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"
     />
-
     <title>Hello, world!</title>
   </head>
   <body>
     <div class="container-fluid p-0">
         <div class="row">
             <div class="col">
-                <form action="/contact" method='POST'>
-                    <div class="mt-3">
-                        <input type="text" class="form-control" placeholder="Ad" name='name'>
-                        <input type="text" class="form-control mt-3" placeholder="Soyad" name='password'>
+                <form action="/welcome" method='POST'>
+                    <div class="m-3 d-flex justify-content-center align-items-center flex-column">
+                        <input type="text" class="form-control" placeholder="Username" name='name'>
+                        <input type="password" class="form-control mt-3" placeholder="password" name='password'>
                         <input type="submit" class="btn btn-primary mt-4" value='Register'>
                     </div>
                 </form>
             </div>
         </div>
     </div>
-
     <!-- Optional JavaScript; choose one of the two! -->
-
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.0-beta1/js/bootstrap.bundle.min.js"></script>
-
     <!-- Option 2: Separate Popper and Bootstrap JS -->
-
     <!-- https://cdnjs.com/libraries/popper.js/2.5.4 -->
     <!-- <script
       src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.5.4/umd/popper.min.js"
@@ -52,11 +49,33 @@ form = '''
     <script
       src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.0-beta1/js/bootstrap.min.js"
     ></script> -->
-
     <!-- More: https://getbootstrap.com/docs/5.0/getting-started/introduction/ -->
 </body>
 </html>
 '''
+c= conn.cursor()
+
+# c.execute(""" CREATE TABLE users (
+#     id integer,
+#     username text,
+#     passw text
+# )""")
+
+# c.execute("INSERT INTO users VALUES ('1', 'huseyn', 'huseyn1234')")
+
+print(c.execute("SELECT * FROM users"))
+result = c.fetchall()
+print(result,'result')
+username = result[0][1]
+p = result[0][2]
+print(p,username)
+
+
+
+conn.commit()
+
+conn.close()
+
 
 
 def render_template(template_name='contact.html', context={}):
@@ -70,6 +89,7 @@ def app(environ, start_response):
     html = form
 
     if environ['REQUEST_METHOD'] == 'POST':
+        print('qwqww')
         post_env = environ.copy()
         post_env['QUERY_STRING'] = ''
         post = cgi.FieldStorage(
@@ -77,15 +97,29 @@ def app(environ, start_response):
             environ=post_env,
             # keep_blank_values=True
         )
-        html = post['name'].value
-        
-        print(html)
-
+        user = post['name'].value
+        password = post['password'].value
+        print(user,password)
+     
+            
+        # try:
+        if user ==  username  and password == p:
+            print('correct',user)
+            start_response('200 OK', [('Content-Type', 'text/html')])
+    
+            data =  render_template(template_name="welcome.html",context={"path": user})
+            data = data.encode()
+            return[data]
+        else:
+            print("not correct")
     start_response('200 OK', [('Content-Type', 'text/html')])
-    # return [html]
+    
     data =  render_template(template_name="contact.html",context={"path": html})
     data = data.encode()
     return [data]
+        # except:
+        #     print(html,'No data')
+
 
 
 
@@ -98,118 +132,3 @@ if __name__ == '__main__':
         httpd.serve_forever()
     except KeyboardInterrupt:
         print('Goodbye.')
-
-
-#-----------------------------------------
-
-
-# import cgi
-
-# class User():
-#     def __init__(self,_id,_name):
-#         self.id=_id
-#         self.name=_name
-
-# users=[
-
-# ]
-
-
-
-# def home(environ):
-#     return render_template(
-#         template_name='index.html', 
-#         context={}
-#     )
-
-# def contact_us(environ):
-#     return render_template(
-#         template_name='contact.html', 
-#         context={}
-#     )
-
-# def app(environ, start_response):
-#     path = environ.get("PATH_INFO")
-#     print(path)
-#     # form = cgi.FieldStorage()
-#     # name = environ.get("PATH_INFO")
-#     # print(name)
-#     if path.endswith("/"):
-#         path = path[:-1]
-
-#     if path == "": 
-        
-#         data = home(environ)
-#         if path != "":
-#             path = environ.get("PATH_INFO")[12:]
-#             print(path)
-#     else:
-#         data = render_template(template_name="contact.html",context={"path": path})
-#     data = data.encode("utf-8")
-#     start_response(
-#         f"200 OK", [
-#             ("Content-Type", "text/html"),
-#             ("Content-Length", str(len(data)))
-#         ]
-#     )
-#     return iter([data])
-
-
-
-# # from urllib.parse import urlparse
-# # from urllib.parse import parse_qs
-
-# # url = 'http://localhost:8080/app.py?name=salam'
-# # parsed_url = urlparse(url)
-# # captured_value = parse_qs(parsed_url.query)['name'][0]
-
-# # print(captured_value)
-
-# if __name__ == '__main__':
-#     try:
-#         from wsgiref.simple_server import make_server
-#         httpd = make_server('', 8080, app)
-#         print('Serving on port 8080...')
-#         httpd.serve_forever()
-#     except KeyboardInterrupt:
-#         print('Goodbye.')
-
-#-----------------------------------------------------
-
-
-# from wsgiref.simple_server import make_server
- 
- 
-# def new():
-#      ret_str = '<html><head></head><body><h1 style="color:blue;">liuhonglei is so handsome</h1></body></html>'
-#      return [ret_str.encode('utf-8')]
- 
- 
-# def index():
-#      ret_str = '/index'
-#      return [ret_str.encode('utf-8')]
- 
- 
-# URLS = {
-#      "/new": new,
-#      "/index": index
-#  }
- 
- 
-# def RunServer(environ, start_response):
-#      start_response('200 OK', [('Content-Type', 'text/html')])
-#      url = environ['PATH_INFO']
-#      if url in URLS.keys():
-#          func_name = URLS[url]
-#          ret = func_name()
-#      else:
-#          ret = '404'.encode('utf-8')
-#      return ret
- 
-# if __name__ == '__main__':
-#      httpd = make_server('', 8000, RunServer)
-#      httpd.serve_forever()
-
-
-
-
